693143403997528df7f5290163b87ce718c21d8e
Tanvi <tanvik27sept@gmail.com>
2021-10-10 22:39:18 +0530

    Add project files.

---
+++ NRCM/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NRCM.Startup))]
+namespace NRCM
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
